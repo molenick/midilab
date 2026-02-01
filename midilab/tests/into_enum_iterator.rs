04e7f384a54c793d312aa4f385f4dc7d0d7f9e05
mo <molenick@users.noreply.github.com>
2026-01-31 17:26:01 -0800

    Remove legacy naming

--- midilab/tests/into_enum_iterator.rs
+++ midilab/tests/into_enum_iterator.rs
@@ -2,7 +2,7 @@ use midilab::IntoEnumIterator;
 use midilab::manufacturer::akai::mpd226::control::value_kind::PadKind;
 
 #[test]
-fn into_enum_iterator_is_usable_from_midi_device() {
+fn into_enum_iterator_is_usable_from_midilab() {
     let variants: Vec<PadKind> = PadKind::iter().collect();
     assert_eq!(variants, vec![PadKind::Note, PadKind::Prog, PadKind::Bank]);
 }
