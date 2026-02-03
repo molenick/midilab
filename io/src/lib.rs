f4bf6351d9afdb4e8bdaedd93e450ce788296cbd
mo <molenick@users.noreply.github.com>
2026-02-02 20:16:28 -0800

    Remove sysex from the equation - we're just doing raw bytes - there's no associated command

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -48,25 +48,41 @@ pub async fn recv_device_bytes(
 pub mod fs {
     use std::path::Path;
 
+    use bytemuck::PodCastError;
+    use midilab::manufacturer::akai;
+    use midilab::manufacturer::akai::mpd226::Preset;
+    use midilab::manufacturer::akai::mpd226::error::PresetDeserializationError;
     use midilab::manufacturer::akai::mpd226::raw::RawPreset;
-    use midilab::manufacturer::akai::mpd226::{self};
-    use midilab::sysex::Sysex;
 
     #[derive(Debug, thiserror::Error)]
     pub enum Error {
         #[error(transparent)]
         FileSys(#[from] std::io::Error),
+        #[error(transparent)]
+        RawPresetDeserialization(#[from] PodCastError),
+        #[error(transparent)]
+        PresetDeserialization(#[from] PresetDeserializationError),
     }
 
-    pub async fn save_akai_mpd226_preset_as_sysex(
-        preset: mpd226::Preset,
+    /// Saves the raw bytes of a preset to the specified path
+    pub async fn save_akai_mpd226_preset(
+        preset: akai::mpd226::Preset,
         path: &Path,
     ) -> Result<(), Error> {
         let raw = RawPreset::from(&preset);
         let payload = bytemuck::bytes_of(&raw).to_vec();
-        let sysex = Sysex::new(payload);
-        let bytes = sysex.as_bytes();
 
-        Ok(tokio::fs::write(path, bytes).await?)
+        Ok(tokio::fs::write(path, payload).await?)
+    }
+
+    /// Loads the raw bytes of a preset from the specified path
+    pub async fn load_akai_mpd226_preset_from_sysex(
+        path: &Path,
+    ) -> Result<akai::mpd226::Preset, Error> {
+        let bytes = tokio::fs::read(path).await?;
+        let raw: RawPreset = *bytemuck::try_from_bytes(&bytes)?;
+        let preset = Preset::try_from(raw)?;
+
+        Ok(preset)
     }
 }
