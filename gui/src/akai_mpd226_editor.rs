9778f6bae61afea1a2950f153065b81a3ed6bd80
mo <molenick@users.noreply.github.com>
2026-02-14 14:32:49 -0800

    Reduce combobox dupes

--- gui/src/akai_mpd226_editor.rs
+++ gui/src/akai_mpd226_editor.rs
@@ -42,7 +42,6 @@ use midilab::message::UiMsg;
 use midilab::message::UserMsg;
 use midilab::midi::Note;
 use midilab::music::ChordRowSequence;
-use midilab::music::ChordVoicing;
 use midilab::music::NotePattern;
 use midilab::music::Octave;
 use midilab::music::PitchClass;
@@ -448,46 +447,22 @@ fn render_note_mapping(ui: &mut Ui, ui_state: &mut UiState) {
                 NotePattern::Scale(seq) => {
                     ui.horizontal(|ui| {
                         ui.label("Tonic");
-                        ComboBox::from_id_salt("note_mapping_tonic")
-                            .selected_text(seq.tonic.to_string())
-                            .show_ui(ui, |ui| {
-                                for p in PitchClass::iter() {
-                                    ui.selectable_value(&mut seq.tonic, p, p.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "note_mapping_tonic", &mut seq.tonic, None);
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Scale");
-                        ComboBox::from_id_salt("note_mapping_scale")
-                            .selected_text(seq.scale.to_string())
-                            .show_ui(ui, |ui| {
-                                for s in ScaleKind::iter() {
-                                    ui.selectable_value(&mut seq.scale, s, s.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "note_mapping_scale", &mut seq.scale, None);
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Octave");
-                        ComboBox::from_id_salt("note_mapping_octave")
-                            .selected_text(seq.octave.to_string())
-                            .show_ui(ui, |ui| {
-                                for s in Octave::iter() {
-                                    ui.selectable_value(&mut seq.octave, s, s.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "note_mapping_octave", &mut seq.octave, None);
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Direction");
-                        ComboBox::from_id_salt("note_mapping_direction")
-                            .selected_text(seq.direction.to_string())
-                            .show_ui(ui, |ui| {
-                                for s in SequenceDirection::iter() {
-                                    ui.selectable_value(&mut seq.direction, s, s.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "note_mapping_direction", &mut seq.direction, None);
                     });
 
                     ui.horizontal(|ui| {
@@ -499,57 +474,27 @@ fn render_note_mapping(ui: &mut Ui, ui_state: &mut UiState) {
                 NotePattern::ChordRow(seq) => {
                     ui.horizontal(|ui| {
                         ui.label("Tonic");
-                        ComboBox::from_id_salt("chord_row_tonic")
-                            .selected_text(seq.tonic.to_string())
-                            .show_ui(ui, |ui| {
-                                for p in PitchClass::iter() {
-                                    ui.selectable_value(&mut seq.tonic, p, p.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "chord_row_tonic", &mut seq.tonic, None);
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Scale");
-                        ComboBox::from_id_salt("chord_row_scale")
-                            .selected_text(seq.scale.to_string())
-                            .show_ui(ui, |ui| {
-                                for s in ScaleKind::iter() {
-                                    ui.selectable_value(&mut seq.scale, s, s.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "chord_row_scale", &mut seq.scale, None);
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Octave");
-                        ComboBox::from_id_salt("chord_row_octave")
-                            .selected_text(seq.octave.to_string())
-                            .show_ui(ui, |ui| {
-                                for o in Octave::iter() {
-                                    ui.selectable_value(&mut seq.octave, o, o.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "chord_row_octave", &mut seq.octave, None);
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Voicing");
-                        ComboBox::from_id_salt("chord_row_voicing")
-                            .selected_text(seq.voicing.to_string())
-                            .show_ui(ui, |ui| {
-                                for v in ChordVoicing::iter() {
-                                    ui.selectable_value(&mut seq.voicing, v, v.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "chord_row_voicing", &mut seq.voicing, None);
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Direction");
-                        ComboBox::from_id_salt("chord_row_direction")
-                            .selected_text(seq.direction.to_string())
-                            .show_ui(ui, |ui| {
-                                for d in SequenceDirection::iter() {
-                                    ui.selectable_value(&mut seq.direction, d, d.to_string());
-                                }
-                            });
+                        enum_combo_box(ui, "chord_row_direction", &mut seq.direction, None);
                     });
 
                     ui.horizontal(|ui| {
@@ -694,14 +639,12 @@ fn render_color_pattern_editor(ui: &mut Ui, id_prefix: &str, pattern: &mut Color
             let mut to_remove: Option<usize> = None;
             for (idx, seq) in sequences.iter_mut().enumerate() {
                 ui.horizontal(|ui| {
-                    ComboBox::from_id_salt(format!("{}_{}_color", id_prefix, idx))
-                        .width(80.0)
-                        .selected_text(seq.color.to_string())
-                        .show_ui(ui, |ui| {
-                            for c in PadColor::iter() {
-                                ui.selectable_value(&mut seq.color, c, c.to_string());
-                            }
-                        });
+                    enum_combo_box(
+                        ui,
+                        format!("{}_{}_color", id_prefix, idx),
+                        &mut seq.color,
+                        Some(80.0),
+                    );
 
                     ui.label("x");
                     ui.add(DragValue::new(&mut seq.len).range(1..=64));
@@ -878,7 +821,6 @@ fn render_all_control_banks(
     fader_repo: &mut FaderRepository,
     switch_repo: &mut SwitchRepository,
 ) {
-    // painting to force layout
     ui.horizontal(|ui| {
         for bank_label in CONTROL_BANKS.iter() {
             let (rect, _) =
@@ -1190,6 +1132,23 @@ fn row_edit_note(ui: &mut Ui, name: &str, value: &mut Note) {
         *value = note;
     }
     ui.end_row();
+}
+
+fn enum_combo_box<T>(ui: &mut Ui, id: impl Into<String>, value: &mut T, width: Option<f32>)
+where
+    T: IntoEnumIterator + std::fmt::Display + Clone + Copy + PartialEq,
+{
+    let mut combo = ComboBox::from_id_salt(id.into()).selected_text(format!("{}", value));
+
+    if let Some(w) = width {
+        combo = combo.width(w);
+    }
+
+    combo.show_ui(ui, |ui| {
+        for variant in T::iter() {
+            ui.selectable_value(value, variant, format!("{}", variant));
+        }
+    });
 }
 
 fn row_edit_enum<T>(ui: &mut Ui, name: &str, value: &mut T)
